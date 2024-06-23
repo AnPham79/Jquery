636e8d9331bf2071e2b1dbb8029dd2615fd9479e
AnPham79 <anpnb79@gmail.com>
2024-06-24 02:19:44 +0700

    hoc jquery udemy

---
+++ practice/02.01.fading/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".red-box").fadeTo(1000, 0.2);
+    $(".green-box").fadeTo(2000, 0.5);
+    $(".blue-box").fadeTo(3000, 0.8);
+
+    $(".blue-box").fadeToggle();
+    $(".blue-box").fadeToggle();
+});
